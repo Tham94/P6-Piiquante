1cdf94e043573da600f136b9943cb320458ae500
Tham94 <tham_the_indian@msn.com>
2022-06-27 19:16:52 +0200

    app.js : set headers and user routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,6 @@
 const express = require("express");
-
+const userRoutes = require("./routes/user");
 require("dotenv").config();
-console.log(process.env);
-console.log();
-console.log();
 
 const mongoose = require("mongoose");
 mongoose
@@ -16,6 +13,17 @@ mongoose
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
 app.use(express.json());
+app.use("/api/auth/", userRoutes);
 
 module.exports = app;
