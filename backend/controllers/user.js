8197f819ed2e7ffd38f5d6169f917625f66887ff
Tham94 <tham_the_indian@msn.com>
2022-06-28 19:45:19 +0200

    secret key salted with base64 on login controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -32,9 +32,13 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
-            }),
+            token: jwt.sign(
+              { userId: user._id },
+              "UkFORE9NX1RPS0VOX1NFQ1JFVA==", // salt the secret key with base64
+              {
+                expiresIn: "24h",
+              }
+            ),
           });
         })
         .catch((error) => res.status(500).json({ error }));
