57e241f02f728423ebfa165237eda7529d62fd0b
Tham94 <tham_the_indian@msn.com>
2022-07-03 13:39:33 +0200

    change on sauce model & remove auth on user routes

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,20 +1,17 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
 
 const sauceSchema = mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
+  userId: { type: String, required: true },
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  usersLiked: { type: [String], required: true },
-  usersDisliked: { type: [String], required: true },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  usersLiked: { type: [String] },
+  usersDisliked: { type: [String] },
 });
-
-userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Sauce", sauceSchema);
