869268a727698ff8858aea870c0e9d16e3cf000a
Tham94 <tham_the_indian@msn.com>
2022-07-05 17:41:54 +0200

    add password validator middleware for signup route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userCtrl = require("../controllers/user");
+const passwordValidator = require("../middleware/password-validator");
 
 const router = express.Router();
 
-router.post("/signup", userCtrl.signup);
+router.post("/signup", passwordValidator, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 module.exports = router;
